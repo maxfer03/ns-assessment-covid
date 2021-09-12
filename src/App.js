2632489ea6ebb51ede168403a0bf9aeb8032000b
maxfer03 <sandsferdinand@gmail.com>
2021-09-12 10:32:48 -0300

    started frontend development

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import axios from "axios";
+import React from "react";
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Landing } from "./views/Landing";
+
 
 function App() {
-  return <div className="App">NICASOURCE</div>;
+
+  return (
+    <div className="App">
+      <Route exact path = '/' component = {Landing}/>
+    </div>
+  );
 }
 
 export default App;
 
-// AXIOS REQUEST:
-
 /* 
-axios.get('http://localhost:3001/', {
+  axios requests examples
+
+    console.log("click");
+    axios
+      .post("https://vast-basin-26481.herokuapp.com/auth/signup", {
+        data: {
+          username: "maxfer12345",
+          password: "123456",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+
+
+
+    axios.get('https://vast-basin-26481.herokuapp.com/stats/all', {
       headers: {
-        "X-JWT-Token": "token"
+        "X-JWT-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6Im1heCIsImRhdGVDcmVhdGVkIjoxNjMxNDUyNDQ3NzA4LCJpc3N1ZWQiOjE2MzE0NTI0NDc3MDgsImV4cGlyZXMiOjE2MzE0NTMzNDc3MDh9.7kNV2EAijwEVpUNvJbb3sM1-UMBTkRK7NVNZugqHmRBrNivQ766mnyBsIHagAQ7Ik5fk3npZMaewh8f98hGFLQ"
       }
     })
+    .then(res => console.log(res))
+
+
 */
---
+++ src/views/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { LogIn } from "../components/LogIn";
+import { Register } from "../components/Register";
+
+export const Landing = () => {
+  return(
+    <div>
+        <div>landing</div>
+        <LogIn/>
+        <Register/>
+    </div>
+    
+  )
+  
+}
