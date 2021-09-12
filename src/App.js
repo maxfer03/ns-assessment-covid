dcfdbcab41ffb18ff4e777aa71b337ebb9fa53ee
maxfer03 <sandsferdinand@gmail.com>
2021-09-12 09:34:15 -0300

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      ns covid
-    </div>
-  );
+  return <div className="App">NICASOURCE</div>;
 }
 
 export default App;
+
+// AXIOS REQUEST:
+
+/* 
+axios.get('http://localhost:3001/', {
+      headers: {
+        "X-JWT-Token": "token"
+      }
+    })
+*/
