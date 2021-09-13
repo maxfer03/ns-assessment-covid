614c89af7cfe7f1749779094f14841c9bd5cd462
maxfer03 <sandsferdinand@gmail.com>
2021-09-13 10:47:45 -0300

    added styles to forbidden

--- src/views/Forbidden.js
+++ src/views/Forbidden.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Box, Button } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 export const Forbidden = () => {
-    return (
-        <div>
-            Forbidden.
-        </div>
-    )
-}
+  const history = useHistory();
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="90vh"
+    >
+      <Box
+        padding={2}
+        border={2}
+        borderColor="primary.main"
+        borderRadius="borderRadius"
+        boxShadow={3}
+      >
+        <Box>Forbidden.</Box>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => history.push("/")}
+        >
+          Log in
+        </Button>
+      </Box>
+    </Box>
+  );
+};
